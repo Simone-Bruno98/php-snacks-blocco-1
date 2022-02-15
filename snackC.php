90015d0dae4ac77ac286000c15788e5ee9964ba8
Simone-Bruno98 <bruno-simone@outlook.it>
2022-02-15 17:53:57 +0100

    Repo impostata

---
+++ snackC.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP SNACK C</title>
+</head>
+<body>
+<!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+</body>
+</html>
