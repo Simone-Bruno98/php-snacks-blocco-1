6a4bc168a06e39343e0f84a83d261270292d375e
Simone-Bruno98 <bruno-simone@outlook.it>
2022-02-16 06:48:53 +0100

    SnackB finito

--- snackB.php
+++ snackB.php
@@ -7,7 +7,22 @@
     <title>PHP SNACK B</title>
 </head>
 <body>
-<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
-    che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+    <?php 
+        // Passare come parametri GET name, mail e age
+        $nome = $_GET["name"];
+        $email = $_GET["mail"];
+        $eta = $_GET["age"];
+
+        // verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
+        // che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”        
+        if (strlen($nome)>3 && strpos($email,"@") !== false && strpos($email,".") !== false && is_numeric($eta) ){
+            echo "<h1>"."Accesso riuscito"."</h1>";
+        }else{
+            echo "<h1>"."Accesso negato"."</h1>";
+        }
+    ?>
 </body>
 </html>
+
+<!-- strlen($nome)>3 -->
